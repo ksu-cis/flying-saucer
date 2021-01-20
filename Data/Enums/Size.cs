03fc593da6702a2dc78f59dfb2db2a6c4fd8b770
Nathan Bean <nhbean@users.campus>
2021-01-20 15:02:47 -0600

    Initial Commit

---
+++ Data/Enums/Size.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author: Nathan Bean
+ * File name: Size.cs
+ * Purpose: To define possible sizes for menu items
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyingSaucer.Data.Enums
+{
+    /// <summary>
+    /// The sizes available for drinks and sides
+    /// </summary>
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
---
+++ DataTests/Enums/SizeTests.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Author: Nathan Bean
+ * File name: Size.cs
+ * Purpose: To unit test the size enumeration
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FlyingSaucer.Data.Enums;
+
+namespace FlyingSaucer.DataTests.Enums
+{
+    /// <summary>
+    /// A class contianing unit tests for the Size enumeration
+    /// </summary>
+    public class SizeTests
+    {
+        [Theory]
+        [InlineData("Small")]
+        [InlineData("Medium")]
+        [InlineData("Large")]
+        public void HasExpectedAttributes(string expected)
+        {
+            Assert.Contains(Enum.GetNames(typeof(Size)), actual => actual == expected);
+        }
+
+        [Fact]
+        public void HasThreeAttributes()
+        {
+            Assert.Equal(3, Enum.GetNames(typeof(Size)).Length);
+        }
+    }
+}
